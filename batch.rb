39d7719f834208c56051e35bb88feca9005d3119
pollseed <cb.so.devope+0001@gmail.com>
2016-01-28 17:56:09 +0900

    Add batch process but should do test.  #10

--- batch.rb
+++ batch.rb
@@ -1,10 +1,32 @@
 require 'bundler/setup'
 require './request.rb'
 
-req = Request.new
-req.argv_run
-#for num in 1..2 do
-#  req.run(java num 100)
-#end
+module Languages
+  JAVA = "java"
+  RUBY = "ruby"
+  JAVASCRIPT = "javascript"
+  PHP = "php"
+  PYTHON = "python"
+  C = "c"
+  C_SHARP = "c#"
+  C_PLUS2 = "c++"
+  OBJECTIVE_C = "objective-c"
+  SWIFT = "swift"
+  SCALA = "scala"
+  LISP = "lisp"
+end
 
+class Batch
+  def run
+    req = Request.new
+    Languages.constants.each{|c|
+      lang = Languages.const_get(c)
+      range = 1..10
+      range.each{|n|
+        req.run(lang,n,100)
+      }
+    }
+  end
+end
 
+Batch.new.run
