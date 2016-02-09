63755616fad6d558f392a021fd1e63f95a8b59d0
pollseed <cb.so.devope+0001@gmail.com>
2016-02-09 11:55:32 +0900

    Add validation #24
    TODO test

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class Server < Sinatra::Base
   end
 
   get '/github' do
+    return json '{status: 404}' unless validation params
     json ClawlGithubRepository.find_limit(
       params['organization_flg'],
       params['order_by'],
@@ -19,6 +20,7 @@ class Server < Sinatra::Base
   end
 
   get '/github/language/:language' do
+    return json '{status: 404}' unless validation params
     json ClawlGithubRepository.find_by_language_limit(
       params['language'],
       params['organization_flg'],
@@ -29,6 +31,29 @@ class Server < Sinatra::Base
   end
 
   get '/github/id/:id' do
+    return json '{status: 404}' unless validation params
     json ClawlGithubRepository.select_column.find_by(github_id: params['id'])
+  end
+
+  private
+  def validation params
+    unless params['language'].nil?
+      return false unless Languages.const_defined?(params['language'])
+    end
+    unless params['organization_flg'].nil?
+      return false unless JsonApi::Flg.const_defined?(params['organization_flg'])
+    end
+    unless params['order_by'].nil?
+      return false unless JsonApi::OrderByColumn.const_defined?(params['order_by'])
+    end
+    unless params['desc_flg'].nil?
+      return false unless JsonApi::Flg.const_defined?(params['desc_flg'])
+    end
+    unless params['limit'].nil?
+      return false unless params['limit'].instance_of?(Fixnum)
+    end
+    unless params['id'].nil?
+      return false unless params['id'].instance_of?(Integer)
+    end
   end
 end
